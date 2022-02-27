d280d1e043063c1519200db48794753d3c5bbc54
Yeom hyera <75641790+Yeomhyera@users.noreply.github.com>
2022-02-27 19:38:25 +0900

    Update 1281-subtract-the-product-and-sum-of-digits-of-an-integer.java

--- 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.java
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.java
@@ -1,6 +1,6 @@
 class Solution {
     public int subtractProductAndSum(int n) {
-        int product = 1, sum = 0;
+        int product = 1, sum = 0;// 곱셈 0 으로하면 끝까지 0. 
         while (n > 0) {
             int num = n % 10;
             product *= num;
