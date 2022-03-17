93cf89adf0d190e0b40808ecab900a8fd55889be
Yeom hyera <75641790+Yeomhyera@users.noreply.github.com>
2022-03-17 22:47:25 +0900

    Time: 0 ms (100.00%), Space: 40.6 MB (64.14%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxDepth(String s) {
+        int answer = 0;
+        int count = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '(')
+                count++;
+            else if(s.charAt(i) == ')')
+                count--;
+            if(answer < count){
+                answer = count;
+            }
+        }
+        return answer;
+    }
+}
